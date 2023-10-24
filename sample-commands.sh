5cfca0f3c1b0ac59f1f5195b00c875674463b732
Christoph Held <christoph.held@allianz.de>
2023-10-24 11:54:45 +0000

    added autoscaling sample command

--- sample-commands.sh
+++ sample-commands.sh
@@ -5,5 +5,8 @@ curl -X POST 'http://localhost:80/acquire?type=aws-account&state=dirty&dest=busy
 # Usage with boskosctl
 boskosctl acquire --server-url http://localhost  --owner-name christoph --type manual-token --target-state used --state free
 
+# Acquire resource with autoscaling -> use timout flag
+boskosctl acquire --server-url http://localhost  --owner-name christoph --type autoscaling-env --target-state used --state free --timeout 1m
+
 # Usage with kubectl
 kubectl get resources -n boskos
