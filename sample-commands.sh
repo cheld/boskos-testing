2a42fb2795aeff033b4acfe78c980cb64e8718f6
Christoph Held <christoph.held@allianz.de>
2023-10-26 08:22:30 +0000

    renamed test environment

--- sample-commands.sh
+++ sample-commands.sh
@@ -3,7 +3,7 @@
 curl -X POST 'http://localhost:80/acquire?type=aws-account&state=dirty&dest=busy&owner=christoph'
 
 # Usage with boskosctl
-boskosctl acquire --server-url http://localhost  --owner-name christoph --type manual-token --target-state used --state free
+boskosctl acquire --server-url http://localhost  --owner-name christoph --type test-environment --target-state used --state free
 
 # Acquire resource with autoscaling -> use timout flag
 boskosctl acquire --server-url http://localhost  --owner-name christoph --type autoscaling-env --target-state used --state free --timeout 1m
